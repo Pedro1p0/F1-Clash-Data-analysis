293b6215891d863739ae23d4012c0f7a8f8cb30a
PedroHenrique18 <86120743+PedroHenrique18@users.noreply.github.com>
2023-10-18 10:48:31 -0300

    Add files via upload

--- networkX.py
+++ networkX.py
@@ -2,6 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import car_setup as cs
 import car_generetor as cg
+import seaborn as sns
 
 # Suponha que você já gerou todas as combinações de carros e tem a lista de Team Scores
 team_scores = [car.car_atributes()[-1] for car in cg.all_cars]
@@ -32,3 +33,20 @@ plt.figure(figsize=(12, 8))
 node_colors = ['blue' if G.nodes[node]['type'] == 'car' else 'red' for node in G.nodes]
 nx.draw(G, pos, with_labels=True, font_weight='bold', node_color=node_colors)
 plt.savefig('grafo.png')
+
+ #Calcular o "Out Degree" de cada nó no grafo
+out_degrees = dict(G.degree(G.nodes(), weight=None))
+
+# Converter os valores de Out Degree em uma lista
+out_degree_values = list(out_degrees.values())
+
+# Criar um histograma com seaborn e traçar a PDF com o KDE
+plt.figure(figsize=(12, 8))
+sns.histplot(out_degree_values, kde=True, color='blue')
+
+plt.xlabel('Out Degree')
+plt.ylabel('PDF')
+plt.title('PDF do Out Degree dos Vértices')
+
+plt.savefig('out_degree_pdf.png')
+plt.show()
